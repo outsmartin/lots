8ced8c81b7d3522ae988b990627eea3d1bc4ab53
dreadlordshark <bloodiestshark@yahoo.com>
2012-04-03 16:53:36 -0300

    line 4 edit of encounter.rb in models, attempt to fix the validation error.

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,7 +1,7 @@
 class Encounter < ActiveRecord::Base
   belongs_to :raid
   validates_uniqueness_of :url
-  validates :url,:format => { :with => /http:\/\/www.kongregate.com\/games\/5thPlanetGames\/legacy-of-a-thousand-suns\?kv_action_type=raidhelp&kv_difficulty=[0-9]&kv_hash=.+&kv_raid_boss=.+&kv_raid_id=[0-9]+/, message: "Valid Kongregate URL needed!" }
+  validates :url,:format => { :with => /http:\/\/www.kongregate.com\/games\/5thPlanetGames\/legacy-of-a-thousand-suns\?kv_action_type=raidhelp&(kv_difficulty=[0-9]&kv_hash=.+&kv_raid_boss=.+&kv_raid_id=[0-9]+/|kv_raid_id=[0-9]+/&kv_difficulty=[0-9]&kv_raid_boss=.+&kv_hash=.+/), message: "Valid Kongregate URL needed!" }
 
   def raid_name
     self.raid.to_s
