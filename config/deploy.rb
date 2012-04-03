3b2588a81d9438287868d8db6e10466f3957e87f
Martin Schneider <m.schneider@gspower.de>
2012-04-04 01:18:01 +0200

    config stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,26 +9,27 @@ set :user ,"root"
 set :use_sudo, false
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
-
-namespace :deploy do
+namespace :thin do
   desc "Start the Thin processes"
   task :start do
     run  <<-CMD
-      cd /var/www/apps/lots/current; bundle exec thin start -p 4001 -e production -d
+      cd #{current_path}; bundle exec thin start -C config/thin.yml
     CMD
   end
 
   desc "Stop the Thin processes"
   task :stop do
     run <<-CMD
-      cd /var/www/apps/lots/current; bundle exec thin stop
+      cd #{current_path}; bundle exec thin stop -C config/thin.yml
     CMD
   end
 
   desc "Restart the Thin processes"
   task :restart do
     run <<-CMD
-      cd /var/www/apps/lots/current; bundle exec thin restart
+      cd #{current_path}; bundle exec thin restart -C config/thin.yml
     CMD
   end
+
 end
+after 'deploy', "thin:restart"
