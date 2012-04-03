aa1da99589b223d2ed70c603046c0b8ef01f1103
Martin Schneider <m.schneider@gspower.de>
2012-04-04 01:38:54 +0200

    config..

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,18 @@ set :user ,"root"
 set :use_sudo, false
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ config/application.rb | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
 namespace :thin do
   desc "Start the Thin processes"
   task :start do
