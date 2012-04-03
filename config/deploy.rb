9a43228e33944cf138e41dbc11bee72d7bac2f1c
Martin Schneider <m.schneider@gspower.de>
2012-04-04 00:58:46 +0200

    config changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,25 @@ set :use_sudo, false
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    run  <<-CMD
+      cd /var/www/apps/lots/current; bundle exec thin start -p 4001 -e production -d
+    CMD
+  end
+
+  desc "Stop the Thin processes"
+  task :stop do
+    run <<-CMD
+      cd /var/www/apps/lots/current; bundle exec thin stop
+    CMD
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    run <<-CMD
+      cd /var/www/apps/lots/current; bundle exec thin restart
+    CMD
+  end
+end
