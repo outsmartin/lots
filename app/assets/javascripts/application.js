7662580873931632cfb996290c2c0d0a05650492
Martin Schneider <m.schneider@gspower.de>
2012-04-15 21:27:05 +0200

    ..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-//= require_tree .
-//= require_self
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui-1.8.18.custom.min
 //= require jquery-ui-timepicker-addon
+//= require_tree .
+//= require_self
 //= require rails.validations
 //= requir twitter/bootstrap
